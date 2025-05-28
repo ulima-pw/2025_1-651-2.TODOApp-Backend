d8b81bf1670b597b439aa8b0782144b18f6c0f64
ProfA1502 <ProfA1502@lab.aca>
2025-05-28 12:40:58 -0500

    TRabajando con todos dinamicos

---
+++ src/data.ts
@@ -0,0 +1,15 @@
+export interface TODO {
+    id : number
+    descripcion : string
+}
+
+export const listaTODOs : TODO[] = [
+    {
+        id : 1,
+        descripcion : "dormir"
+    },
+    {
+        id : 2,
+        descripcion : "comer"
+    }
+]
--- src/index.ts
+++ src/index.ts
@@ -1,21 +1,42 @@
 import express, { Request, Response } from "express"
 import dotenv from "dotenv"
+import { listaTODOs, TODO } from "./data"
+
 
 dotenv.config()
 const app = express()
 
 const PORT = process.env.PORT
 
+
 app.get("/", (req : Request, resp : Response) => {
     resp.send("Endpoint raiz")
 })
 
 app.get("/todos", (req : Request, resp : Response) => {
-    const todos = [
-        { id : 1, descripcion : "Preparar la comida"},
-        { id : 2, descripcion : "Estudiar"}
-    ]
+    const todos = listaTODOs
     resp.json(todos)
+})
+
+app.get("/todos/:id", (req : Request, resp : Response) => {
+    const id = req.params.id
+
+    let todoEncontrado : TODO | null = null
+    for (let todo of listaTODOs) {
+        if (todo.id.toString() == id) {
+            todoEncontrado = todo
+            break;
+        }
+    }
+
+    if (todoEncontrado == null) {
+        // Error: no se encontro
+        resp.status(400).json({
+            msg : "Codigo incorrecto"
+        })
+    }
+
+    resp.json(todoEncontrado)
 })
 
 app.listen(PORT, () => {
