6170847e634da68d5eb1ebeae60bae14e6be2f59
ProfA1502 <ProfA1502@lab.aca>
2025-05-28 12:11:09 -0500

    Documentacion de 2do endpoint

--- Readme.md
+++ Readme.md
@@ -9,9 +9,22 @@
 - Input : -
 - Output
 
+json
 ```
 [
     { "id" : 1, "descripcion" : "desc de todo 1" },
     { "id" : 2, "descripcion" : "desc de todo 2" },
 ]
+```
+
+### Obtener TODO
+
+- Path : "/todos"
+- Metodo : GET
+- Input : "/:id" : Id del TODO.
+- Output
+
+json
+```
+    { "id" : 1, "descripcion" : "desc de todo 1" }
 ```
--- src/index.ts
+++ src/index.ts
@@ -10,6 +10,14 @@ app.get("/", (req : Request, resp : Response) => {
     resp.send("Endpoint raiz")
 })
 
+app.get("/todos", (req : Request, resp : Response) => {
+    const todos = [
+        { id : 1, descripcion : "Preparar la comida"},
+        { id : 2, descripcion : "Estudiar"}
+    ]
+    resp.json(todos)
+})
+
 app.listen(PORT, () => {
     console.log(`Se inicio servidor en puerto ${PORT}`)
 })
